955c8e85d4973ee030891f236cc2a55f6ae2f18b
Tomáš Horecký <xhorecky@fi.muni.cz>
2017-03-15 00:08:06 +0100

    Updated polybar confix, now with more poorly visible text!

--- .config/i3/lock.sh
+++ .config/i3/lock.sh
@@ -6,7 +6,7 @@ tmpbg='/tmp/screen.png'
 (( $# )) && { icon=$1; }
 
 scrot "$tmpbg"
-convert "$tmpbg" -scale 50% -blur 0x5 -scale 200% $tmpbg
+convert "$tmpbg" -scale 40% -blur 0x8 -scale 250% $tmpbg
 #-scale 5% -scale 2000% "$tmpbg"
 convert "$tmpbg" "$icon" -gravity center -composite -matte "$tmpbg"
 i3lock -u -i "$tmpbg"
